b10f1df6a3d2d3bf7ec57a7263959fa9143846e2
Sergey Shmakov <31436321+kuqmua@users.noreply.github.com>
2023-06-29 20:58:50 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#[proc_macro_derive()]
+pub fn (input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    proc_macro_helpers::panic_location::panic_location();//panic_location function from https://github.com/kuqmua/proc_macro_helpers
+    let ast: syn::DeriveInput =
+        syn::parse(input).unwrap_or_else(|_| panic!("let ast: syn::DeriveInput = syn::parse(input) failed"));
+    let ident = &ast.ident;
+    match ast.data {
+        syn::Data::Union(_) => panic!("does not work on union!"),
+        syn::Data::Struct(_) => panic!("does not work on structs!"),
+        syn::Data::Enum(_) => panic!("does not work on enums!"),
+    }
+    let gen = quote::quote! {};
+    //println!("{gen}");
+    gen.into()
+}
